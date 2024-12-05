dd80f0f2dee6a813c76213c8a3532a4829e5fab5
BuzyBeeC <brianc.gde@gmail.com>
2024-12-04 21:54:40 -0800

    Revert "Adding f0.g grid file"
    This reverts commit c1f3e1dbd0de59d60e1b591b1f8ace14ed35d95c.

--- src/grid.ts
+++ src/grid.ts
@@ -152,17 +152,4 @@ export class Grid extends StatisticSubject {
     }
     return count;
   }
-  //checks for plants at x growth level
-  getGrowthLevel(level: number): number {
-    let count = 0;
-    for(let y = 0; y < this.height; y++){
-      for(let x = 0; x < this.width; x++){
-        const plant = this.getPlant({x,y});
-        if(plant && plant.growthLevel > level){
-          count++
-        }
-      }
-    }
-    return count;
-  }
 }
