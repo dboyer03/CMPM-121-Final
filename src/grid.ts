dboyer03/CMPM-121-Final

commit d2a881cc7d656e49dc02c2b26fc720435f55e90d

Merge plant data into grid byte array to better align with f1.a. Revamp save system to remove inefficiencies double serialization and saving config with every checkpoint. Store current game config in StateManager.

src/grid.ts
@@ -1,15 +1,20 @@
 // grid
 
 import { Position } from "./position.ts";
-import { canGrow, Plant, PlantTypeInfo } from "./plant.ts";
-import { Cell } from "./cell.ts";
+import { canGrow, Plant, PlantType, PlantTypeInfo } from "./plant.ts";
 import { StatisticSubject, StatisticTracker } from "./statistic.ts";
+
+interface Cell {
+  water: number;
+  sunlight: number;
+}
+
+const CELL_PLANT_WIDTH = 4;
 
 export class Grid extends StatisticSubject {
   readonly width: number;
   readonly height: number;
   private state: Uint8Array; // Byte array to store grid state
-  private plants: Map<string, Plant>;
   private readonly INTERACTION_RANGE = 1; // adjacent cell range
   private readonly MAX_WATER = 5; // max water
   private readonly MAX_RAIN = 2; // max water increase per tick
@@ -20,21 +25,23 @@ export class Grid extends StatisticSubject {
     super(statTracker);
     this.width = width;
     this.height = height;
-    this.state = new Uint8Array(width * height * 2); // Each cell has water and sunlight
-    this.plants = new Map();
+    this.state = new Uint8Array(width * height * CELL_PLANT_WIDTH);
+    // Each cell has water, sunlight, plant type, and growth level as 1 byte each
 
     // Initialize state
-    for (let i = 0; i < this.state.length; i += 2) {
+    for (let i = 0; i < this.state.length; i += CELL_PLANT_WIDTH) {
       this.state[i] = 0; // water
       this.state[i + 1] = 0; // sunlight
+      this.state[i + 2] = PlantType.NONE; // plant type
+      this.state[i + 3] = 0; // growth level
     }
   }
 
   private getIndex(pos: Position): number {
-    return (pos.y * this.width + pos.x) * 2;
+    return (pos.y * this.width + pos.x) * CELL_PLANT_WIDTH;
   }
 
-  getCellProperties(pos: Position): Cell {
+  getCell(pos: Position): Cell {
     const index = this.getIndex(pos);
     return {
       water: this.state[index],
@@ -42,10 +49,24 @@ export class Grid extends StatisticSubject {
     };
   }
 
-  setCellProperties(pos: Position, cell: Cell): void {
+  getPlant(pos: Position): Plant {
+    const index = this.getIndex(pos);
+    return {
+      type: this.state[index + 2],
+      growthLevel: this.state[index + 3],
+    };
+  }
+
+  setCell(pos: Position, cell: Cell): void {
     const index = this.getIndex(pos);
     this.state[index] = cell.water;
     this.state[index + 1] = cell.sunlight;
+  }
+
+  setPlant(pos: Position, plant: Plant): void {
+    const index = this.getIndex(pos);
+    this.state[index + 2] = plant.type;
+    this.state[index + 3] = plant.growthLevel;
   }
 
   getState(): Uint8Array {
@@ -56,24 +77,15 @@ export class Grid extends StatisticSubject {
     this.state = state;
   }
 
-  getPlants(): Map<string, Plant> {
-    return this.plants;
-  }
-
-  setPlants(plants: Map<string, Plant>): void {
-    this.plants = plants;
-  }
-
   updateEnvironment(): void {
     let livingPlants = 0;
 
     // update cell resources
     for (let y = 0; y < this.height; y++) {
       for (let x = 0; x < this.width; x++) {
-        const pos = { x, y };
-        const key = this.getPlantKey(pos);
-        const cell = this.getCellProperties(pos);
-        const plant = this.plants.get(key);
+        const pos: Position = { x, y };
+        const cell: Cell = this.getCell(pos);
+        const plant: Plant = this.getPlant(pos);
 
         // update water (rain)
         if (cell.water < this.MAX_WATER) {
@@ -90,7 +102,9 @@ export class Grid extends StatisticSubject {
         cell.sunlight = Math.floor(Math.random() * (this.MAX_SUNLIGHT + 1));
 
         // update plant
-        if (plant) {
+        if (
+          plant.type !== PlantType.NONE && plant.type !== PlantType.WITHERED
+        ) {
           // update plant growth
           if (canGrow(plant, cell.water, cell.sunlight)) {
             plant.growthLevel++;
@@ -104,14 +118,14 @@ export class Grid extends StatisticSubject {
           ) {
             // kill plant
             this.statisticTracker.increment("plantDied", plant.type); // notify before modification
-            this.plants.set(key, { type: "withered", growthLevel: 1 });
+            this.setPlant(pos, { type: PlantType.WITHERED, growthLevel: 1 });
           } else {
             // plant didn't die
             livingPlants++;
           }
         }
 
-        this.setCellProperties(pos, cell);
+        this.setCell(pos, cell);
       }
     }
     this.statisticTracker.setIfMax("maxGridAlive", livingPlants);
@@ -123,42 +137,38 @@ export class Grid extends StatisticSubject {
     return dx <= this.INTERACTION_RANGE && dy <= this.INTERACTION_RANGE;
   }
 
-  sowPlant(pos: Position, type: string): boolean {
-    const key = this.getPlantKey(pos);
-    const cell = this.getCellProperties(pos);
-    const plant = {
+  sowPlant(pos: Position, type: PlantType): boolean {
+    const cell = this.getCell(pos);
+    const newPlant: Plant = {
       type,
       growthLevel: 1,
     };
-    if (!this.plants.has(key) && canGrow(plant, cell.water, cell.sunlight)) {
-      this.plants.set(key, plant);
+    if (
+      (this.getPlant(pos).type === PlantType.NONE) &&
+      canGrow(newPlant, cell.water, cell.sunlight)
+    ) {
+      this.setPlant(pos, newPlant);
       cell.water -= PlantTypeInfo[type].waterToGrow;
       this.statisticTracker.increment("plantSown", type);
-      this.setCellProperties(pos, cell);
+      this.setCell(pos, cell);
       return true;
     }
     return false;
   }
 
   reapPlant(pos: Position): Plant | null {
-    const key = this.getPlantKey(pos);
-    const plant = this.plants.get(key);
-    if (plant && plant.growthLevel === PlantTypeInfo[plant.type].maxGrowth) {
-      this.plants.delete(key);
-      if (plant.type !== "withered") {
+    const plant = this.getPlant(pos);
+    if (
+      (this.getPlant(pos).type !== PlantType.NONE) &&
+      (plant.growthLevel === PlantTypeInfo[plant.type].maxGrowth)
+    ) {
+      this.setPlant(pos, { type: PlantType.NONE, growthLevel: 0 });
+      if (plant.type !== PlantType.WITHERED) {
         this.statisticTracker.increment("plantReaped", plant.type);
       }
       return plant;
     }
     return null;
-  }
-
-  getPlantKey(pos: Position): string {
-    return `${pos.x},${pos.y}`;
-  }
-
-  getPlant(pos: Position): Plant | undefined {
-    return this.plants.get(this.getPlantKey(pos));
   }
 
   isValidPosition(pos: Position): boolean {
@@ -173,7 +183,7 @@ export class Grid extends StatisticSubject {
         if (
           this.isValidPosition({ x, y }) &&
           !(x === pos.x && y === pos.y) &&
-          this.getPlant({ x, y })
+          (this.getPlant({ x, y }).type !== PlantType.NONE)
         ) {
           count++;
         }

src/cell.ts
@@ -1,6 +1,0 @@
-// cell
-
-export interface Cell {
-  water: number;
-  sunlight: number;
-}

src/main.ts
@@ -1,6 +1,6 @@
 // ====== Main + Imports ======
 import { Game, GameConfig, StateManager } from "./state.ts";
-import { PlantAction, PlantTypeInfo } from "./plant.ts";
+import { getCssName, PlantAction, PlantType, PlantTypeInfo } from "./plant.ts";
 
 import "./style.css";
 import "./game.css";
@@ -22,8 +22,8 @@ const title = document.createElement("h1");
 title.textContent = GAME_NAME;
 
 // ====== Initialize Game State ======
-const stateManager: StateManager = new StateManager();
-let game: Game = stateManager.newGame(GAME_CONFIG);
+const stateManager: StateManager = new StateManager(GAME_CONFIG);
+let game: Game = stateManager.newGame();
 
 // ====== Game Scoring ======
 function calculateScore(): number {
@@ -51,7 +51,7 @@ function updateGridDisplay(): void {
       cell.className = "grid-cell";
       const pos = { x, y };
 
-      const cellProperties = game.grid.getCellProperties(pos);
+      const cellProperties = game.grid.getCell(pos);
 
       // water segments (top o cell)
       for (let i = 0; i < cellProperties.water; i++) {
@@ -70,10 +70,11 @@ function updateGridDisplay(): void {
       }
 
       const plant = game.grid.getPlant(pos);
-      if (plant) {
+      if (plant.type !== PlantType.NONE) {
         const plantElement = document.createElement("div");
-        plantElement.className =
-          `plant type-${plant.type} level-${plant.growthLevel}`;
+        plantElement.className = `plant type-${
+          getCssName(plant.type)
+        } level-${plant.growthLevel}`;
         cell.appendChild(plantElement);
       }
 
@@ -111,7 +112,7 @@ gameHud.id = "game-hud";
 
 const plantTypeDisplay = document.createElement("div");
 plantTypeDisplay.className = "current-plant-type";
-const initialTypeName = "Green Circle"; // default plant type
+const initialTypeName = PlantTypeInfo[game.player.getCurrentPlantType()].name;
 plantTypeDisplay.textContent = `Current Plant: ${initialTypeName}`;
 gameHud.appendChild(plantTypeDisplay);
 
@@ -239,12 +240,9 @@ undoButton.onclick = () => {
   if (undoState !== null) {
     game = undoState;
     updateAllDisplays();
-  } else if (stateManager.isInitialized()) {
+  } else {
     game = stateManager.getInitialState()!;
     alert("Reset to initial state. No more days to undo.");
-  } else {
-    alert("No more days to undo.");
-    console.error("StateManger not initialized");
   }
 };
 
@@ -288,7 +286,7 @@ const instructions = document.createElement("div");
   // TODO: Describe mechanics and limitations (e.g. player reach, plant growth, water/sunlight, etc.)
   description.innerText =
     `Click the cells current or adjacent to the farmer to sow or reap plants. \
-    Click the Finish Day button to end your turn, advance the day, and autosave. \
+    Click the Finish Day button to end your turn, advance the day, and autosave a checkpoint. \
     You can use the Undo and Redo Checkpoint buttons if you want to replay a day/checkpoint. \
     You can also manually create and load saves mid-day, creating more checkpoints for extra granularity. \
     \n
@@ -339,17 +337,17 @@ document.addEventListener("keydown", (e) => {
       break;
     case "1":
     case "num1":
-      game.player.setPlantType("green-circle");
+      game.player.setPlantType(PlantType.GREEN_CIRCLE);
       updatePlantSelect();
       break;
     case "2":
     case "num2":
-      game.player.setPlantType("yellow-triangle");
+      game.player.setPlantType(PlantType.YELLOW_TRIANGLE);
       updatePlantSelect();
       break;
     case "3":
     case "num3":
-      game.player.setPlantType("purple-square");
+      game.player.setPlantType(PlantType.PURPLE_SQUARE);
       updatePlantSelect();
       break;
   }

src/plant.ts
@@ -2,7 +2,7 @@
 
 const MAX_GROWTH = 3;
 
-export interface PlantType {
+export interface FullPlantType {
   name: string;
   maxGrowth: number;
   waterToGrow: number;
@@ -10,36 +10,55 @@ export interface PlantType {
   maxCrowding: number;
 }
 
-export const PlantTypeInfo: { [key: string]: PlantType } = {
-  "green-circle": {
+export enum PlantType { // indices map to PlantTypeInfo
+  NONE = 255, // max value of 1 byte for grid storage
+  GREEN_CIRCLE = 0,
+  YELLOW_TRIANGLE,
+  PURPLE_SQUARE,
+  WITHERED = 3, // ALWAYS add new types at the end to avoid changing indices
+}
+
+export const PlantTypeInfo: FullPlantType[] = [
+  {
     name: "Green Circle",
     maxGrowth: MAX_GROWTH,
     waterToGrow: 2,
     sunToGrow: 2,
     maxCrowding: 8,
   },
-  "yellow-triangle": {
+  {
     name: "Yellow Triangle",
     maxGrowth: MAX_GROWTH,
     waterToGrow: 1,
     sunToGrow: 3,
     maxCrowding: 2,
   },
-  "purple-square": {
+  {
     name: "Purple Square",
     maxGrowth: MAX_GROWTH,
     waterToGrow: 3,
     sunToGrow: 1,
     maxCrowding: 4,
   },
-  "withered": {
+  {
     name: "withered",
     maxGrowth: 1,
     waterToGrow: Infinity,
     sunToGrow: Infinity,
     maxCrowding: Infinity,
   },
-};
+]; // ALWAYS add new types at the end to avoid changing indices
+
+/** Turn plant name into css-friendly name */
+export function getCssName(type: PlantType): string {
+  if (type === PlantType.NONE) {
+    return "none";
+  }
+  return PlantTypeInfo[type].name
+    .split(" ")
+    .map((word) => word[0].toLowerCase() + word.slice(1))
+    .join("-");
+}
 
 export enum PlantAction {
   SOW,
@@ -47,7 +66,7 @@ export enum PlantAction {
 }
 
 export interface Plant {
-  type: string;
+  type: PlantType;
   growthLevel: number;
 }
 

src/player.ts
@@ -2,13 +2,13 @@
 
 import { Position } from "./position.ts";
 import { Grid } from "./grid.ts";
-import { PlantAction } from "./plant.ts";
+import { PlantAction, PlantType } from "./plant.ts";
 import { StatisticSubject, StatisticTracker } from "./statistic.ts";
 
 export class Player extends StatisticSubject {
   private position: Position;
   private grid: Grid;
-  private currentPlantType: string = "green-circle";
+  private currentPlantType: PlantType = PlantType.GREEN_CIRCLE;
 
   constructor(grid: Grid, startPos: Position, statTracker: StatisticTracker) {
     super(statTracker);
@@ -46,11 +46,11 @@ export class Player extends StatisticSubject {
     return { ...this.position };
   }
 
-  setPlantType(type: string) {
+  setPlantType(type: PlantType) {
     this.currentPlantType = type;
   }
 
-  getCurrentPlantType(): string {
+  getCurrentPlantType(): PlantType {
     return this.currentPlantType;
   }
 

src/state.ts
@@ -2,7 +2,7 @@ import { StatisticName, StatisticTracker } from "./statistic.ts";
 import { Grid } from "./grid.ts";
 import { Player } from "./player.ts";
 import { DayManager } from "./day.ts";
-import { Plant } from "./plant.ts";
+import { PlantType } from "./plant.ts";
 import { Position } from "./position.ts";
 
 export interface GameConfig {
@@ -12,79 +12,63 @@ export interface GameConfig {
 }
 
 export interface Game {
-  readonly config: GameConfig;
   statTracker: StatisticTracker;
   grid: Grid;
   dayManager: DayManager;
   player: Player;
 }
 
-interface SaveData {
-  config: GameConfig;
-  statistics: [StatisticName, (number | [string, number][])][];
-  gridState: number[];
-  plants: [string, Plant][];
+interface Checkpoint {
+  statistics: [StatisticName, (number | [PlantType, number][])][];
+  gridState: number[]; // array of 4-byte structs represent cell and plant states
   playerPosition: Position;
   dayCount: number;
 }
 
+interface SaveData {
+  config: GameConfig;
+  history: Checkpoint[];
+}
+
 export class StateManager {
   static readonly AUTO_SLOT: string = "auto";
-  private history: string[];
-  private current: number; // points to index of current save state=
+  private history: Checkpoint[];
+  private current: number; // points to index of current save state
+  private config: GameConfig;
 
-  constructor() {
+  constructor(config: GameConfig) {
     this.history = [];
     this.current = -1;
+    this.config = config;
   }
 
-  isInitialized(): boolean {
+  isGameLoaded(): boolean { // auto-initializes after running newGame
     return this.history.length > 0 && this.current >= 0;
   }
 
-  newGame(config: GameConfig): Game {
-    this.history = [];
-
-    const game: Partial<Game> = { config };
-    game.statTracker = new StatisticTracker();
-    game.grid = new Grid(config.gridWidth, config.gridHeight, game.statTracker);
-    game.dayManager = new DayManager(game.grid);
-    game.player = new Player(game.grid, { x: 0, y: 0 }, game.statTracker);
-
-    const fullGame: Game = game as Game;
-    this.pushToHistory(fullGame); // save initial state
-
-    return fullGame;
-  }
-
-  private static serialize(game: Game): string {
-    const saveData: SaveData = {
-      config: game.config,
+  private static createCheckpoint(game: Game): Checkpoint {
+    return {
       statistics: game.statTracker.getStatisticsArray(),
       gridState: Array.from(game.grid.getState()),
-      plants: Array.from(game.grid.getPlants()),
       playerPosition: game.player.getPosition(),
       dayCount: game.dayManager.getCurrentDay(),
     };
-    return JSON.stringify(saveData);
   }
 
-  private static deserialize(data: string): Game {
-    const saveData: SaveData = JSON.parse(data);
-    const game: Partial<Game> = { config: saveData.config };
+  private loadCheckpoint(checkpoint: Checkpoint): Game {
+    const game: Partial<Game> = {};
 
-    game.statTracker = new StatisticTracker(saveData.statistics);
+    game.statTracker = new StatisticTracker(checkpoint.statistics);
     game.grid = new Grid(
-      saveData.config.gridWidth,
-      saveData.config.gridHeight,
+      this.config!.gridWidth,
+      this.config!.gridHeight,
       game.statTracker,
     );
-    game.grid.setState(new Uint8Array(saveData.gridState));
-    game.grid.setPlants(new Map(saveData.plants));
-    game.dayManager = new DayManager(game.grid, saveData.dayCount, true);
+    game.grid.setState(new Uint8Array(checkpoint.gridState));
+    game.dayManager = new DayManager(game.grid, checkpoint.dayCount, true);
     game.player = new Player(
       game.grid,
-      saveData.playerPosition,
+      checkpoint.playerPosition,
       game.statTracker,
     );
 
@@ -96,31 +80,51 @@ export class StateManager {
       // if it exists, remove future
       this.history.splice(this.current + 1, Infinity);
     }
-    this.history.push(StateManager.serialize(game));
+    this.history.push(StateManager.createCheckpoint(game));
     this.current = this.history.length - 1;
     return this;
   }
 
+  newGame(config?: GameConfig): Game {
+    if (config !== undefined) config = this.config;
+    this.history = [];
+
+    const game: Partial<Game> = {};
+    game.statTracker = new StatisticTracker();
+    game.grid = new Grid(
+      this.config.gridWidth,
+      this.config.gridHeight,
+      game.statTracker,
+    );
+    game.dayManager = new DayManager(game.grid);
+    game.player = new Player(game.grid, { x: 0, y: 0 }, game.statTracker);
+
+    const fullGame: Game = game as Game;
+    this.pushToHistory(fullGame); // save initial state
+
+    return fullGame;
+  }
+
   getInitialState(): Game | null {
-    return (this.history.length > 0)
-      ? StateManager.deserialize(this.history[0])
-      : null;
+    if (this.isGameLoaded()) {
+      return this.loadCheckpoint(this.history[0]);
+    }
+    throw new Error("StateManager not initialized.");
   }
 
   getUndo(): Game | null {
     if (this.current >= 0) {
-      const previousState: string = this.history[--this.current];
-      return StateManager.deserialize(previousState);
+      return this.loadCheckpoint(this.history[--this.current]);
     }
     return null;
   }
 
   getRedo(): Game | null {
     if (
-      (this.history.length > 0) && ((this.current + 1) < this.history.length)
+      this.history.length > 0 &&
+      ((this.current + 1) < this.history.length)
     ) {
-      const nextState = this.history[++this.current];
-      return StateManager.deserialize(nextState);
+      return this.loadCheckpoint(this.history[++this.current]);
     }
     return null;
   }
@@ -132,9 +136,12 @@ export class StateManager {
 
   trySaveGame(game: Game, slot: string): boolean {
     this.pushToHistory(game);
-    const serializedHistory = JSON.stringify(this.history);
+    const saveData: SaveData = {
+      config: this.config,
+      history: this.history,
+    };
     try {
-      localStorage.setItem(`save_${slot}`, serializedHistory);
+      localStorage.setItem(`save_${slot}`, JSON.stringify(saveData));
     } catch (e) {
       if (e instanceof DOMException && e.name === "QuotaExceededError") {
         return false;
@@ -156,13 +163,17 @@ export class StateManager {
   }
 
   tryLoadSave(slot: string): Game | null {
-    const saveData = localStorage.getItem(`save_${slot}`);
+    const jsonSave: string | null = localStorage.getItem(`save_${slot}`);
+    if (jsonSave === null) return null;
+
+    const saveData: SaveData = JSON.parse(jsonSave);
     if (saveData === null) return null;
-    this.history = JSON.parse(saveData);
+    this.config = saveData.config;
+    this.history = saveData.history;
     this.current = this.history.length - 1;
 
-    return (this.isInitialized())
-      ? StateManager.deserialize(this.history[this.current])
+    return (this.isGameLoaded())
+      ? this.loadCheckpoint(this.history[this.current])
       : null;
   }
 }

src/statistic.ts
@@ -1,10 +1,12 @@
+import { PlantType } from "./plant.ts";
+
 export type StatisticName =
   | "playerTraveled" // steps taken
   | "maxGridAlive" // max number of live plants on the grid
   | "plantSown"
   | "plantReaped"
   | "plantDied";
-type StatisticValue = number | Map<string, number>;
+type StatisticValue = number | Map<PlantType, number>;
 
 /** A class for tracking game statistics. */
 export class StatisticTracker {
@@ -13,13 +15,13 @@ export class StatisticTracker {
   /**
    * Create a StatisticTracker.
    */
-  constructor(statistics?: [StatisticName, number | [string, number][]][]) {
+  constructor(statistics?: [StatisticName, number | [PlantType, number][]][]) {
     if (statistics) {
       // Reconstruct from array form
       this.statistics = new Map<StatisticName, StatisticValue>(
         statistics.map(([key, value]) => {
           if (Array.isArray(value)) {
-            return [key, new Map<string, number>(value)];
+            return [key, new Map<PlantType, number>(value)];
           }
           return [key, value];
         }),
@@ -28,17 +30,17 @@ export class StatisticTracker {
       this.statistics = new Map<StatisticName, StatisticValue>([
         ["playerTraveled", 0],
         ["maxGridAlive", 0],
-        ["plantSown", new Map<string, number>()],
-        ["plantReaped", new Map<string, number>()],
-        ["plantDied", new Map<string, number>()],
+        ["plantSown", new Map<PlantType, number>()],
+        ["plantReaped", new Map<PlantType, number>()],
+        ["plantDied", new Map<PlantType, number>()],
       ]);
     }
   }
 
   /** Returns a copy of all statistics as array */
-  getStatisticsArray(): [StatisticName, number | [string, number][]][] {
+  getStatisticsArray(): [StatisticName, number | [PlantType, number][]][] {
     // Perform deep copy and conversion to array
-    const copy = new Map<StatisticName, (number | [string, number][])>();
+    const copy = new Map<StatisticName, (number | [PlantType, number][])>();
     for (const [key, value] of this.statistics) {
       copy.set(key, (typeof value === "number") ? value : Array.from(value));
     }
@@ -48,7 +50,7 @@ export class StatisticTracker {
   }
 
   /** Gets the value of a statistic. */
-  get(statistic: StatisticName, key?: string): number | null {
+  get(statistic: StatisticName, key?: PlantType): number | null {
     const value = this.statistics.get(statistic);
     if (value === undefined) {
       return null;
@@ -72,7 +74,7 @@ export class StatisticTracker {
   }
 
   /** Increments a statistic by 1. */
-  increment(statistic: StatisticName, key?: string): void {
+  increment(statistic: StatisticName, key?: PlantType): void {
     const value = this.statistics.get(statistic);
     if (value === undefined) {
       return;
@@ -87,7 +89,7 @@ export class StatisticTracker {
   }
 
   /** Sets a max statistic if the new value is greater than the current value. */
-  setIfMax(statistic: StatisticName, value: number, key?: string): void {
+  setIfMax(statistic: StatisticName, value: number, key?: PlantType): void {
     const currentValue = this.statistics.get(statistic); // checks if stat exists
     if (currentValue !== undefined) {
       if (typeof currentValue === "number") { // checks if stat is a number

commit 3cd1f19f2c7be876e90f42d2a1288a1e783b14e2

Hotfix: add missing plant setter in environment update broken in d2a881cc when converting plant from object into array bytes

src/grid.ts
@@ -121,6 +121,7 @@ export class Grid extends StatisticSubject {
             this.setPlant(pos, { type: PlantType.WITHERED, growthLevel: 1 });
           } else {
             // plant didn't die
+            this.setPlant(pos, plant);
             livingPlants++;
           }
         }